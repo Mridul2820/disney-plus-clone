9d48fafe83fa8f59c96e95a35a571920cfd2a825
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-06 21:23:47 +0530

    adding movies from firebase

--- src/components/NewDisney.js
+++ src/components/NewDisney.js
@@ -1,32 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from "react-router-dom";
+import MovieCard from './MovieCard';
+import { useSelector } from 'react-redux';
+import { selectNewDisney } from "../features/movie/movieSlice";
 
 const NewDisney = () => {
+    const movies = useSelector(selectNewDisney);
+
     return (
         <StyledContainer>
             <h4>New to Disney+</h4>
             <StyledContent>
-                <StyledWrap>
-                    <Link to="/">
-                        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/87F1DCF36049558159913ADFD18A800DE1121771540033EC3A7651B8FE154CEB/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
-                    </Link>
-                </StyledWrap>
-                <StyledWrap>
-                    <Link to="/">
-                        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/87F1DCF36049558159913ADFD18A800DE1121771540033EC3A7651B8FE154CEB/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
-                    </Link>
-                </StyledWrap>
-                <StyledWrap>
-                    <Link to="/">
-                        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/87F1DCF36049558159913ADFD18A800DE1121771540033EC3A7651B8FE154CEB/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
-                    </Link>
-                </StyledWrap>
-                <StyledWrap>
-                    <Link to="/">
-                        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/87F1DCF36049558159913ADFD18A800DE1121771540033EC3A7651B8FE154CEB/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
-                    </Link>
-                </StyledWrap>
+            {movies && 
+                movies.map((movie, key) => (
+                    <MovieCard
+                        key={key} 
+                        movieID={movie.id}
+                        movieImg={movie.cardImg}
+                        movieTitle={movie.title}
+                    />
+                ))
+            }
             </StyledContent>
         </StyledContainer>
     )
@@ -43,38 +37,6 @@ const StyledContent = styled.div`
 
     @media (max-width: 768px) {
         grid-template-columns: repeat(2, minmax(0, 1fr));
-    }
-`
-
-const StyledWrap = styled.div`
-    padding-top: 56.25%;
-    border: 3px solid rgba(249, 249, 249, 0.1);
-    border-radius: 10px;
-    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    cursor: pointer;
-    overflow: hidden;
-    position: relative;
-    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-
-    img {
-        inset: 0px;
-        display: block;
-        height: 100%;
-        object-fit: cover;
-        opacity: 1;
-        position: absolute;
-        transition: opacity 500ms ease-in-out 0s;
-        width: 100%;
-        z-index: 1;
-        top: 0;
-    }
-
-    &:hover {
-        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-        rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-        transform: scale(1.05);
-        border-color: rgba(249, 249, 249, 0.8);
     }
 `
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,63 @@
-import React from 'react'
+import { useEffect } from 'react'
 import styled from 'styled-components'
+
 import ImageSlide from '../components/ImageSlide'
 import NewDisney from '../components/NewDisney'
 import Originals from '../components/Originals'
 import Recommends from '../components/Recommends'
+import Trending from '../components/Trending'
 import Viewers from '../components/Viewers'
 
+import { useDispatch, useSelector } from 'react-redux'
+import db from '../firebase/firebase'
+import { setMovies } from '../features/movie/movieSlice'
+import { selectUserName } from '../features/user/userSlice'
+
 const Home = () => {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trending = [];
+
+    useEffect(() => {
+        // console.log("hello");
+        db.collection("movies").onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                // console.log(recommends);
+                switch (doc.data().type) {
+                case "recommend":
+                    recommends = [...recommends, { id: doc.id, ...doc.data() }];
+                    break;
+        
+                case "new":
+                    newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+                    break;
+        
+                case "original":
+                    originals = [...originals, { id: doc.id, ...doc.data() }];
+                    break;
+        
+                case "trending":
+                    trending = [...trending, { id: doc.id, ...doc.data() }];
+                    break;
+                    
+                default : 
+                }                
+            });
+            dispatch(
+                setMovies({
+                    recommend: recommends,
+                    newDisney: newDisneys,
+                    original: originals,
+                    trending: trending,
+                })
+            );
+        });
+    }, [userName]);
+
     return (
         <StyledContainer>
             <ImageSlide />
